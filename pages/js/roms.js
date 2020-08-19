0222dc83933ca1bcea2cb446aeb452e7b098a575
Tanyiqu <1953649096@qq.com>
2020-08-19 21:11:06 +0800

    pages roms页面基本成型

--- pages/js/roms.js
+++ pages/js/roms.js
@@ -1,7 +1,11 @@
 
 // 入口函数
 $(document).ready(() => {
+    // 加载版本信息
     loadVersions();
+
+    // 点击事件
+    loadEvents();
 });
 
 
@@ -10,14 +14,21 @@ let VERSIONS =
     [{
         name: "究极绿宝石lV.B-VGC2019神战纪念版",
         cover: "imgs/version/4b-vgc.png",
-        downloadUrl: ""
+        downloadUrl: "https://tanyiqu.lanzous.com/iwwWOfttlih"
     }, {
-        name: "究极绿宝石lV-小智版 Impossible Version 通常版",
+        name: "究极绿宝石lV Ash Inverse Version【逆属性修复版】",
+        cover: "imgs/version/4b-xz-nsx.png",
+        downloadUrl: "https://tanyiqu.lanzous.com/i705hti"
+    },
+    {
+        name: "究极绿宝石lV-小智版 Impossible Version【通常版修复2】",
         cover: "imgs/version/4b-xz.png",
-        downloadUrl: ""
+        downloadUrl: "https://tanyiqu.lanzous.com/i705dvg"
+    }, {
+        name: "究极绿宝石lV.B【Index Valution】",
+        cover: "imgs/version/4b.png",
+        downloadUrl: "https://tanyiqu.lanzous.com/i6knpuj"
     }];
-
-
 
 
 /**
@@ -29,8 +40,29 @@ function loadVersions() {
     let html = "";
     let length = VERSIONS.length;
     for (let i = 0; i < length; i++) {
-        html += ' <li><img src="' + VERSIONS[i].cover + '" alt=""><div class="right"><h3 class="version-name">' + VERSIONS[i].name + '</h3></div></li>';
-        console.log(VERSIONS[i].cover);
+        html += `
+        <li>
+            <img class="ver-img"  src="{1}" alt="">
+            <div class="right">
+                <h3 class="version-name">{2}</h3>
+                <p class="version-dl" durl="{3}">下载该版本</p>
+            </div>
+        </li>`.format(i, VERSIONS[i].cover, VERSIONS[i].name, VERSIONS[i].downloadUrl);
     }
     versionList.html(html);
+}
+
+
+// 点击事件
+function loadEvents() {
+    // 图片点击 => 新页面显示此图片
+    $('.ver-img').click((e) => {
+        window.open(e.target.src);
+    });
+
+
+    // 点击下载
+    $('.version-dl').click((e) => {
+        window.open($(e.target).attr('durl'));
+    });
 }
---
+++ pages/js/util.js
@@ -0,0 +1,28 @@
+/**
+ * 工具类
+ */
+
+/**
+* 字符串格式化辅助
+*/
+String.prototype.format = function (args) {
+    let result = this;
+    if (arguments.length > 0) {
+        if (arguments.length === 1 && typeof (args) == "object") {
+            for (let key in args) {
+                if (args.key !== undefined) {
+                    let reg = new RegExp("({" + key + "})", "g");
+                    result = result.replace(reg, args.key);
+                }
+            }
+        } else {
+            for (let i = 0; i < arguments.length; i++) {
+                if (arguments[i] !== undefined) {
+                    let reg = new RegExp("({[" + i + "]})", "g");
+                    result = result.replace(reg, arguments[i]);
+                }
+            }
+        }
+    }
+    return result;
+};
